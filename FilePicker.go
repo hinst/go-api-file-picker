0dcd515d49b8e89bb1f26d6bb10e83fdecb35bc5
Alexander Savinykh <asavinykh@nbrx.de>
2022-01-06 23:13:03 +0100

    Initial

---
+++ FilePicker.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"io/fs"
+	"net/http"
+	"path/filepath"
+)
+
+type FilePicker struct {
+}
+
+type FileInfo struct {
+}
+
+func (picker *FilePicker) GetFileList(responseWriter http.ResponseWriter, request *http.Request) {
+	var directory = request.URL.Query().Get("directory")
+	filepath.Walk(directory, func(path string, info fs.FileInfo, err error) error {
+
+		return nil
+	})
+}
